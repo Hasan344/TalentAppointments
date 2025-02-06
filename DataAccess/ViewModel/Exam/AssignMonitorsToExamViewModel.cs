a8045a992719a0c4e344b5145a55261c4cce8a9d
hesen.i <hesen.i@HASANI-PC>
2025-02-06 11:06:33 +0400

    Error messages added to assigning processes

--- DataAccess/ViewModel/Exam/AssignMonitorsToExamViewModel.cs
+++ DataAccess/ViewModel/Exam/AssignMonitorsToExamViewModel.cs
@@ -5,9 +5,16 @@ namespace ForQab.DataAccess.ViewModel.Exam
     public class AssignMonitorsToExamViewModel
     {
         public int ExamId { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "The number of experts must be at least 1.")]
-        public int NumberOfMonitors { get; set; }
         public int SectionId { get; set; }
+
+        [Required]
+        public List<MonitorAssignmentViewModel> Assignments { get; set; } = new();
+    }
+
+    public class MonitorAssignmentViewModel
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "At least 1 monitor must be assigned.")]
+        public int NumberOfMonitors { get; set; }
         public int GenderId { get; set; }
         public DateOnly MaxDate { get; set; }
     }
