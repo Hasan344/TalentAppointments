ad297535bf8537d8cb1a9e2a226098f88eb84eb3
hesen.i <hesen.i@HASANI-PC>
2025-04-03 10:03:58 +0400

    Adding Financial Rates

---
+++ DataAccess/Models/FinancialRate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForQab.DataAccess.Models;
+
+[Table("Financial_Rates")]
+public partial class FinancialRate
+{
+    [Key]
+    [Column("Id")]
+    public int Id { get; set; }
+
+    [Column("Article")]
+    [StringLength(20)]
+    public string Article { get; set; } = null!;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Rate { get; set; }
